029e67354461ff095935e5c7a1769434a24f07df
Maksym Kazantsev <maksimkazancev12@gmail.com>
2024-06-11 13:21:26 +0200

    fin seccion 12

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 @Module({
@@ -21,6 +23,10 @@ import { FilesModule } from './files/files.module';
       autoLoadEntities:true,
       synchronize:true
     }),
+
+    ServeStaticModule.forRoot({ 
+      rootPath: join(__dirname,'..','public'), 
+      }) ,
 
     ProductsModule,
 
--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, NotFoundException, BadGatewayException } from '@nestjs/common';
+import { Controller, Get, Post,Param, UploadedFile, UseInterceptors,  BadGatewayException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter';
 import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {
-   
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) {}
+
+
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res:Response,
+    @Param('imageName') imageName:string
+  ){
+      const path= this.filesService.getStaticProductImage(imageName);
+
+     res.sendFile(path);
   }
 
   @Post('product')
@@ -15,9 +31,11 @@ export class FilesController {
     fileFilter: fileFilter,
    // 1mb limits:{fileSize:1000}
    storage: diskStorage({
-    destination:'./static/uploads'
+    destination:'./static/uploads',
+    filename:fileNamer,
    })
   }))
+  
   uploadProductImage(
   @UploadedFile()  file:Express.Multer.File
   ){
@@ -26,6 +44,10 @@ export class FilesController {
       throw new BadGatewayException('Make sure that the file is an image')
     }
 
-    return file
+    //const secureUrl = `${file.filename}`;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+
+
+    return {  secureUrl };
   }
 }
